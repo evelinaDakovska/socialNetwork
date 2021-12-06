6bf9393902068c989a7bbe0577b2ac2ed30a5c0e
evelinaDakovska <evelina.dakovska@gmail.com>
2021-12-06 13:46:57 +0200

    Add login, profile and register components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import { Routes, Route } from 'react-router-dom';
+
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import Profile from "./components/Profile/Profile";
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Main/>
+      
+      <Routes>
+        <Route path='/' element={<Main/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+      </Routes>
+
+      
     </div>
   );
 }
