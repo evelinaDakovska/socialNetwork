4f77337852f77baa0ff42f89e6725fc754954ad8
evelinaDakovska <evelina.dakovska@gmail.com>
2021-12-07 11:36:22 +0200

    Create port styling

---
+++ src/components/CreatePost/CreatePost.js
@@ -0,0 +1,53 @@
+import styles from "./CreatePost.module.css";
+
+const CreatePost = () => {
+  const createPostHandler = (e) => {
+    e.preventDefault();
+    console.log("Posted!");
+  };
+
+  return (
+    <div>
+      <form
+        id={styles.createPostForm}
+        method="POST"
+        onSubmit={createPostHandler}
+      >
+        <fieldset>
+          <legend>Your Post</legend>
+          <p className={styles.field}>
+            <input
+              type="text"
+              name="title"
+              id={styles.title}
+              placeholder="Title *"
+            />
+          </p>
+          <p className={styles.field}>
+            <input
+              type="url"
+              name="imageURL"
+              id={styles.imageURL}
+              placeholder="Image URL"
+            />
+          </p>
+          <p className={styles.field}>
+            <input
+              type="text"
+              name="description"
+              id={styles.description}
+              placeholder="Description *"
+            />
+          </p>
+          <input
+            className={styles.buttonSubmit}
+            type="submit"
+            value="CreatePost"
+          />
+        </fieldset>
+      </form>
+    </div>
+  );
+};
+
+export default CreatePost;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,11 +3,13 @@ import { Link } from "react-router-dom";
 import styles from './Main.module.css';
 
 import PostCard from "../PostCard/PostCard";
+import CreatePost from "../CreatePost/CreatePost";
 
 
 const Main=()=>{
 return (
 <div className={styles.main_content}>
+    <CreatePost/>
     <PostCard/>
 </div>
 );
