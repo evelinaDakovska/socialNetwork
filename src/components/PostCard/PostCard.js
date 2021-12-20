e676d5cb7d1c519d44e6e47afa18e9e79e75a26c
evelinaDakovska <evelina.dakovska@gmail.com>
2021-12-20 10:50:35 +0200

    button for profile only vissible on home page

--- src/components/PostCard/PostCard.js
+++ src/components/PostCard/PostCard.js
@@ -5,17 +5,23 @@ import styles from "./PostCard.module.css";
 
 const PostCard = ({ post }) => {
   let user = post.ownerName.split(" ");
+
+  let profileButton = (
+    <Link
+      to={`/user-profile/${post._ownerId}`}
+      title="User Profile"
+      className={styles.button}
+    >
+      {`${user[0]}'s Profile`}
+    </Link>
+  );
+
+  let URL = window.location.pathname;
+
   return (
     <div className={styles.cardContainer}>
       <div className={styles.postCardForm}>
-        <Link
-          to={`/user-profile/${post._ownerId}`}
-          title="User Profile"
-          className={styles.button}
-        >
-          {`${user[0]}'s Profile`}
-        </Link>
-
+        {URL === "/home" ? profileButton : null}
         <Link
           to={`/details/${post._id}`}
           title="Details"
